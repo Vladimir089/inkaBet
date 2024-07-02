a4505f8d7259790209272311444bbe73ebb35651
Vladimir089 <pickerpikcer@gmail.com>
2024-07-02 12:58:46 +0300

    Initial Commit

---
+++ inkaBet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  inkaBet
+//
+//  Created by Владимир Кацап on 02.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
